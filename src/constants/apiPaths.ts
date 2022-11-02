152ca1c846ce2079c584bec67fc9bd5fac24bfb3
neerajkushwaha1993 <neeraj_kushwaha@epam.com>
2022-11-02 14:08:09 +0530

    update import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://slg8f927md.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ap6l89ofv0.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://slg8f927md.execute-api.us-east-1.amazonaws.com/dev/",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
